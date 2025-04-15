71a31648527e656ea18a8165523485f221928b4e
Jade16 <jade_bortot_paiva@usp.br>
2025-04-15 20:32:08 -0300

    added previous folders

---
+++ README.md
@@ -0,0 +1,9 @@
+Repositório destinado ao armazenamento dos relatórios e scripts SQL para a disciplina SCC0541 - Laboratório de Base de Dados 2025 ministrada pelo professor Caetano Traina Junior e montira PAE Érica Peters do Carmo.
+
+Todo o conteúdo desde repositório fui produzido totalmente pelo grupo de alunos:
+
+Nome: Eduardo Ribeiro Rodrigues NUSP: ------79
+Nome: Gustavo Blois NUSP: ------62
+Nome: Jade Bortot de Paiva NUSP: ------83
+Nome: Matheus Godoy Bolsarini NUSP: -----01 
+
--- postgre_sql/flake.nix
+++ postgre_sql/flake.nix
@@ -18,9 +18,16 @@
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
+          #openconnect
           postgresql  
           #pgadmin4 
-        ];
+      ];
+      shellHook = ''
+        echo "É necessário conectando à VPN USP"
+        echo "Para isso, execute: ./scripts/conectar-vpn.sh" 
+        echo "Para conectar no servidor, execute o seguinte comando com devidos dados:"
+        echo "psql -h host -p port -U username -d database"
+      '';
       };
     });
 }
